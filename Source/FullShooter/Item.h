eed77510ea83384cc3dadb9a7c5753d305fa92d0
HyunsangJoo <joohs86@naver.com>
2022-11-15 08:39:29 +0900

    Item UI

---
+++ Source/FullShooter/Item.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Item.generated.h"
+
+UCLASS()
+class FULLSHOOTER_API AItem : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AItem();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadonly, Category = "Item Property", meta = (AllowPrivateAccess = "true"))
+	USkeletalMeshComponent* ItemMesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadonly, Category = "Item Property", meta = (AllowPrivateAccess = "true"))
+	class UBoxComponent* CollisionBox;
+
+	UPROPERTY(EditAnywhere, BlueprintReadonly, Category = "Item Property", meta = (AllowPrivateAccess = "true"))
+	class UWidgetComponent* PickupWidget;
+
+
+public:
+	USkeletalMeshComponent* GetItemMesh() const;
+	UBoxComponent* GetCollisionBox() const;
+};
